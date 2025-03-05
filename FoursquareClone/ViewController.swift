392dea2aebff04e18aec7bfd0d5c93a99785db32
Cansın Memiş <109155533+CansinM@users.noreply.github.com>
2025-03-05 10:39:33 +0300

    Initial Commit

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Cansın Memiş on 5.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FoursquareCloneTests/FoursquareCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoursquareCloneTests.swift
+//  FoursquareCloneTests
+//
+//  Created by Cansın Memiş on 5.03.2025.
+//
+
+import Testing
+@testable import FoursquareClone
+
+struct FoursquareCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
